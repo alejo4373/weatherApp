14a7f912e79c77307ed831a1b46a25eda4ff2f92
alejo4373 <alejo4373@gmail.com>
2018-09-12 16:12:28 -0400

    weather for the week view

--- src/Components/WeekWeatherList.jsx
+++ src/Components/WeekWeatherList.jsx
@@ -3,8 +3,33 @@ import React, { Component } from 'react';
 class WeekWeatherList extends Component {
 
   render() {
+    const { weatherDays, scale } = this.props;
     return (
       <div >
+        <table>{weatherDays.map((day, index) => {
+          let maxTemp = day.maxTempF;
+          let minTemp = day.minTempF;
+          let date = new Date(day.dateTimeISO)
+          let dayOfWeekLong = date.toLocaleDateString('en-US', { weekday: 'long' });
+
+          if (scale !== 'c') {
+            maxTemp = day.maxTempC;
+            minTemp = day.minTempC;
+          }
+
+          if (index !== 0) {
+            return (
+              <tr>
+                <td>{dayOfWeekLong}</td>
+                <td><img src={`icons/${day.icon}`} alt={day.icon} /></td>
+                <td></td>
+                <td>{maxTemp}</td>
+                <td>{minTemp}</td>
+              </tr>
+            )
+          }
+        })
+        }</table>
       </div>
     );
   }
